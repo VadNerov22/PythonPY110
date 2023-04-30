290dad8afb71cbd2ceba0521801b159a6d6be536
VadNerov22 <vadoleg@mail.ru>
2023-05-01 00:12:43 +0300

    initial commit

--- Занятие3/Домашние_задания/task1/main.py
+++ Занятие3/Домашние_задания/task1/main.py
@@ -1,3 +1,7 @@
+def write_list(file1, file2, file3):
+    with open(file1) as f1, open(file2) as f2, open(file3, "w") as f3:
+        return f3.write(f1.read() + "\n" + f2.read())
+
+
 if __name__ == "__main__":
-    # Write your solution here
-    pass
+    write_list("list1.txt", "list2.txt", "list3.txt")
