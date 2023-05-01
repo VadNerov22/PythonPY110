a004ac0a07e19ae14178b04d304443a81268198f
VadNerov22 <vadoleg@mail.ru>
2023-05-01 23:55:04 +0300

    Выполненная ЛР 3 модуля 2.

--- Занятие3/Лабораторные_задания/task2_1/main.py
+++ Занятие3/Лабораторные_задания/task2_1/main.py
@@ -3,7 +3,7 @@ import json
 
 def task() -> str:
     dict_numbers = {k: k ** 2 for k in range(1, 11)}
-    return json.dumps(dict_numbers,indent=4)
+    return json.dumps(dict_numbers, indent=4)
 
 
 if __name__ == "__main__":
